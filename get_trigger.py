a2b0d0713cb3139236d9f68daaf411da4dc6c87e
Duy35P <phantranhuuduy@gmail.com>
2026-06-24 16:25:17 +0700

    delete unnecessary .py

--- get_trigger.py
+++
@@ -1,8 +0,0 @@
-import pyodbc
-
-conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost;DATABASE=luckydraw;UID=sa;PWD=duy123')
-cursor = conn.cursor()
-cursor.execute("SELECT OBJECT_DEFINITION(OBJECT_ID('TRG_CAMPAIGN_VALIDATE'))")
-row = cursor.fetchone()
-if row:
-    print(row[0])
